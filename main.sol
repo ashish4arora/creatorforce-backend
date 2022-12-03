1a8f1e590d3aa1da818c4371d6a96c2835337661
Ashish Arora <ashisharora.dev@gmail.com>
2022-12-03 11:47:44 +0530

    Add files via upload

--- main.sol
+++ main.sol
@@ -1,12 +1,11 @@
 pragma solidity ^0.8.13;
 contract main{
     enum Type{live, recorded};
-    uint liveCourseId;
-    uint liveCourseId;
-    uint recordedCourseId;
+    uint public liveCourseId;
+    uint public recordedCourseId;
 
     constructor (){
-        liveCourseId = 0; //live course ids are even
+        liveCourseId = 2; //live course ids are even
         recordedCourseId = 1; //recorded course ids are odd
     }
 
@@ -24,19 +23,19 @@ contract main{
         String description;
         uint numLectures;
         uint price; //in wei
-        uint[] lectures;
+        Lecture[] lectures;
     }
 
     struct Lecture{
         String title;
         String description;
         String cid; //content id of the associated ipfs file
-        uint parentid;
+        // uint parentid;
     }
 
     //map of courseid with data
-    map(uint => Live) livecourses;
-    map(uint => Recorded) recordedcourses;
+    map(uint => Live) public livecourses; //even mapping
+    map(uint => Recorded) public recordedcourses; //odd mapping
     // we search for a courseid in both livecourses and recordedcourses
 
     //map for user nfts
@@ -44,15 +43,56 @@ contract main{
     //map for creator nfts
     map(address => courseid) public creatornft;
 
-    function createLive(startTime, endTime, title, desc, cid, price){
-        session = Live(startTime, endTime, title, desc, cid, price);
-        
+    function createLive(uint startTime, uint endTime, String title, String desc, String cid, uint price){
+        Live session = Live(startTime, endTime, title, desc, cid, price);
+        map[liveCourseId] = session;
+        liveCourseId += 2;
     }
 
+    //assuming struct of lectures is created using web3 and passed as a parameter
+    function createRecorded(String title, String desc, uint numLectures, uint price, Lecture[] lectures){
+        Recorded session = Recorded(title, desc, numlectures, price, lectures);
+        recordedcourses[recordedCourseId] = session;
+        recordedCourseId += 2;
+    }
+
+    // function createLectures(String title, String description, uint cid) returns (Lecture thelecture){
+    //     Lecture thelecture = Lecture(title, description, cid);
+    //     return thelecture;
+    // }
 
     function buycourse(courseid) public {
         //add modifier to check whether courseid is valid or not
-        if (msg.value >=  )
+        if (courseid % 2 == 0){
+            if (msg.value >= livecourses[courseid].price){
+                address nftaddress; //stores the nft address generated either in frontend or from here?
+                usernft[nftaddress] = courseid;
+            }
+        }else{
+            if (msg.value >= recordedcourses[courseid].price){
+                address nftaddress; //stuff said above
+                usernft[nftadress] = courseid;
+            }
+        }
+    }
+
+    function fetchCourseUser(address[] nftlist) returns (String[] coursesEnrolled){
+        String[] coursesEnrolled = [];
+        for(uint i = 0; i < nftlist.length; i++){  
+            if (usernft[nftlist[i]] > 0){
+                coursesEnrolled.push(courseid);
+            }
+        }
+        return coursesEnrolled;
+    }
+
+    function fetchCourseCreator(address[] nftlist) returns (String[] coursesEnrolled){
+        String[] coursesCreated = [];
+        for(uint i = 0; i < nftlist.length; i++){  
+            if (usernft[nftlist[i]] > 0){
+                coursesCreated.push(courseid);
+            }
+        }
+        return coursesCreated;
     }
 }
-
